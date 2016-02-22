591f5bed5d37f3bf076c4211ece4cbc3526d101f
Tim Mendoza <timmydoza@gmail.com>
2016-02-21 21:08:32 -0800

    add more tests

--- test/mozart_test.js
+++ test/mozart_test.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect;
 var mozart = require(__dirname + '/../index');
+var fs = require('fs');
 
 describe('mozart-dice-game', function() {
   describe('the getMinuet function', function() {
@@ -14,6 +15,21 @@ describe('mozart-dice-game', function() {
     it('should return a valid MIDI file', function() {
       var minuetObj = mozart.getMinuet();
       expect(minuetObj.bytes.slice(0, 4)).to.eql('MThd');
+    });
+  });
+  describe('the saveMinuet function', function() {
+    it('should syncronously save a valid MIDI file to disk if no callback is provided', function() {
+      mozart.saveMinuet(__dirname + '/sync.mid');
+      var midiFile = fs.readFileSync(__dirname + '/sync.mid').toString();
+      expect(midiFile.slice(0,4)).to.eql('MThd');
+    });
+    it('should asyncronously save a valid MIDI file to disk if no callback is provided', function(done) {
+      mozart.saveMinuet(__dirname + '/async.mid', function(err) {
+        expect(err).to.eql(null);
+        var midiFile = fs.readFileSync(__dirname + '/async.mid').toString();
+        expect(midiFile.slice(0,4)).to.eql('MThd');
+        done();
+      });
     });
   });
 });
